39f8a65448d99d268d5f7237f43a6d39e36876f7
LVX77 <vitorgmb2000@hotmail.com>
2021-04-23 13:27:27 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/example/demo/domain/Pedido.java
+++ src/main/java/com/example/demo/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.example.demo.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -33,6 +36,11 @@ public class Pedido implements Serializable{
 	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco endereco;
 
+	
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
+	
 	public Pedido() {
 		
 	}
@@ -98,6 +106,14 @@ public class Pedido implements Serializable{
 		this.endereco = endereco;
 	}
 	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 	
 	
 	@Override
@@ -116,5 +132,6 @@ public class Pedido implements Serializable{
 			return false;
 		return true;
 	}
+
 
 }
