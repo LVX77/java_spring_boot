c8c7ee4b63faa010502e7b7758838bd71fcd2aa8
LVX77 <vitorgmb2000@hotmail.com>
2021-05-01 13:02:19 -0300

    somando preco de cada ItemPedido

--- src/main/java/com/example/demo/domain/Pedido.java
+++ src/main/java/com/example/demo/domain/Pedido.java
@@ -60,15 +60,15 @@ public class Pedido implements Serializable{
 		this.endereco = endereco;
 	}
 
-	
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+	public double getValorTotal() {
+		double soma = 0;
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
 	}
+	
+
 
 
 	
@@ -139,5 +139,11 @@ public class Pedido implements Serializable{
 		return true;
 	}
 
-
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 }
